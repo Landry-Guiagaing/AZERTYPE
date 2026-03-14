b5b76af2c69d499e759910d358886ee31e9de590
Landry-Guiagaing <lguiagaing@gmail.com>
2026-03-14 05:23:13 +0100

    affichage et vérification si correcte de la proposition entré par l'utilisateur

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,4 @@
 lancerJeu();
 
-let inputEcriture = document.getElementById("inputEcriture");
-let btnValiderMot = document.getElementById("btnValiderMot");
-console.log(inputEcriture);
-console.log(btnValiderMot);
-
-let zoneProposition = document.querySelector(".zoneProposition");
-console.log(zoneProposition);
-
-
 let listebtnRadio = document.querySelectorAll(".optionSource input");
 console.log(listebtnRadio);
--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,47 @@
-// déclaration des variables score, motUtilisateur qui vas contenir le mot sasie par l'utiliateur et liste
-    let choix;
-    let score = 0;
-    let listePropositions;
-
-function lancerBoucleDeJeu(choix){
-    let motUtilisateur;
- 
-    // choix des listes en fonction de l'entrer utilisateur
-    if (choix === "mots"){
-        listePropositions = listeMots;
-    } else if (choix === "phrases"){ 
-        listePropositions = listePhrase;
-    }
-
-    //étallage des expression à l'utilisateur et calcul du score
-    for (let i = 0; i < listePropositions.length; i++) {
-        motUtilisateur = prompt("veillez saisir l'expression : " + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            score++;
-        }
-    }
-    return score;
-}
-
-// demande d'interaction à l'utilisateur pour le choix des liste sur les quelle il souhaite s'exercer
-function choisirPhrasesOuMots(){
-    while (choix !== "mots" && choix !== "phrases") { 
-        choix = prompt("choisissez la liste de mots ou de phrases: mots pour une liste de mots et phrases pour une liste de phrases");
-    }
-    return choix;
-}
-
 // affichage du résultat à l'utilisateur
 function afficherResultat(score, nbPropositions){
     let spanScore = document.querySelector(".zoneScore span");
     spanScore.innerHTML = `${score} / ${nbPropositions}`;
 }
 
+function afficherProposition(proposition){
+    let zoneProposition = document.querySelector(".zoneProposition")
+
+    zoneProposition.innerHTML = proposition
+}
+
+
 function lancerJeu(){
-    choisirPhrasesOuMots();
-    lancerBoucleDeJeu(choix);
-    afficherResultat(score, listePropositions.length);
+    let score = 0;
+    let listeProposition = listeMots
+    let i = 0;
+    proposition = listeMots[i]
+
+
+    let btnValider = document.getElementById("btnValiderMot");
+    let inputEcriture = document.getElementById("inputEcriture");
+    afficherProposition(listeProposition[i]);
+
+
+    btnValider.addEventListener("click", () =>{
+        console.log("jai cliquer sur le bouton valider ! ");
+        console.log(inputEcriture.value)
+
+        if (inputEcriture.value === listeProposition[i]){
+            score ++
+        }
+        
+        i++
+        afficherResultat(score, i);
+        inputEcriture.value = '';
+        if (listeProposition[i] === undefined){
+            afficherProposition("Le jeu est fini !")
+            btnValider.desabled = true
+        } else {
+            afficherProposition(listeProposition[i]);
+        }
+
+    })
+
 }
 
